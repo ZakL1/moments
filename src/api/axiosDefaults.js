05c2ddf1a71a8302e292bad771e7ff5f4c166e62
ZakL1 <zakerylyel32@gmail.com>
2025-03-10 18:56:17 +0000

    created sign up and sign in forms

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://django-api-lesson-4f619d0ee306.herokuapp.com/'
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
